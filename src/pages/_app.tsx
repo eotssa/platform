6d85c649466ffb222183f25e6147ac99f69740a1
eotsso <107133870+eotsso@users.noreply.github.com>
2023-12-16 16:47:14 -0500

    Update dependencies and add Mantine UI library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,31 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { type Session } from "next-auth"
+import { SessionProvider } from "next-auth/react"
+import { type AppType } from "next/app"
 
-import { api } from "~/utils/api";
+import { api } from "~/utils/api"
 
-import "~/styles/globals.css";
+import "~/styles/globals.css"
+
+import "@mantine/core/styles.css"
+
+import type { AppProps } from "next/app"
+import { createTheme, MantineProvider } from "@mantine/core"
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+    <MantineProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
